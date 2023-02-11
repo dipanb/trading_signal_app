a06bd07bf57286f91603538d1fe9b8abd69a8677
Dipan1994 <dipanbiswasiitkgp@gmail.com>
2023-02-11 21:43:06 +0530

    Update defs.py

--- constants/defs.py
+++ constants/defs.py
@@ -1,3 +1,3 @@
-API_KEY = "c6cbea0fb8436d3a63eec8700ac4fa76-5779ef2f8c1151739495c4ab99f2df69"
-ACCOUNT_ID = "101-004-24733228-001" 
+API_KEY = "Use your own key"
+ACCOUNT_ID = "Use your own account number" 
 OANDA_URL = "https://api-fxpractice.oanda.com/v3"
